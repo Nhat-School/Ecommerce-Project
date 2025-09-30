b8dbd0ac62ed7e815696d3e262df11ba074fa472
Nhat-School <nhatpv.b23ce074@stu.ptit.edu.vn>
2025-09-30 20:56:57 +0700

    delete feature

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import axios from "axios";
 import {formatMoney} from "../../utils/money";
 import { DeliveryOptions } from "./DeliveryOptions";
 export function OrderSummary({cart, deliveryOption,loadCart}) {
@@ -11,6 +12,10 @@ export function OrderSummary({cart, deliveryOption,loadCart}) {
                             return deliveryOption.id === cartItem.deliveryOptionId;
                           }
                         );
+                        const deleteCartItem = async () =>{
+                            await axios.delete(`/api/cart-items/${cartItem.productId}`);
+                            await loadCart();
+                        };
                         return (
                           <div key={cartItem.productId} className="cart-item-container">
                             <div className="delivery-date">
@@ -43,7 +48,8 @@ export function OrderSummary({cart, deliveryOption,loadCart}) {
                                   <span className="update-quantity-link link-primary">
                                     Update
                                   </span>
-                                  <span className="delete-quantity-link link-primary">
+                                  <span className="delete-quantity-link link-primary"
+                                  onClick={deleteCartItem}>
                                     Delete
                                   </span>
                                 </div>
